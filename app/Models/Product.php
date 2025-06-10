efd93bc5220a6cdc06d42003cd57c9f34354afa7
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-06-10 22:08:00 +0630

    change left join to model binding

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,12 @@ class Product extends Model
         'stock',
         'image'
     ];
+
+    //a product has one category
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+
 }
